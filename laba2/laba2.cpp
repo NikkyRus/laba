43e4fa9e79c7a353edc21a05baccdf7821183d4f
User <User@DESKTOP-HHHJIA5>
2022-01-22 15:21:31 +0300

    Добавьте файлы проекта.

---
+++ laba2/laba2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <clocale>
+
+using namespace std;
+int check, z;
+int main() {
+	
+	  setlocale(LC_ALL, "Russian");
+	
+	cout << "Введите сумму для проверки: ";
+	cin >> check;
+	cout << "Введите размер массива: ";
+	cin >> z;
+	vector <int> a;
+	cout << "Перечислите элементы массива: ";
+	for (int i = 0; i < z; i++) {
+		int mas;
+		cin >> mas;
+		a.push_back(mas);
+	}
+	for (int i = 0; i < z; i++) {
+		for (int j = 0; j < z; j++) {
+			if (a[i] + a[j] == check) {
+				cout << a[i] << ", " << a[j];
+				cout << endl;
+			}
+		}
+	}
+}
